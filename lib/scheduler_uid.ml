ce5c28964dec869ee28022e520438e3016d3ad44
Leandro Ostera <leandro@abstractmachines.dev>
2023-10-13 04:42:05 +0200

    chore: pad scheduler ids

--- lib/scheduler_uid.ml
+++ lib/scheduler_uid.ml
@@ -3,7 +3,7 @@ type t = int
 let __current__ = Atomic.make 0
 let next () = Atomic.fetch_and_add __current__ 1
 let equal a b = Int.equal a b
-let pp ppf t = Format.fprintf ppf "%d" t
+let pp ppf t = Format.fprintf ppf "%02d" t
 let to_int t = t
 
 let reset () =

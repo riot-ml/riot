20b1b8867398525720b68c6838d17cf88e25fccd
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-22 23:57:53 +0100

    fix: expose that socket is a Fd.t

--- riot/lib/net/socket.mli
+++ riot/lib/net/socket.mli
@@ -1,8 +1,8 @@
 open Runtime
 
 type 'kind socket = Fd.t
-type listen_socket = Fd.t
-type stream_socket = Fd.t
+type listen_socket = [ `listen ] socket
+type stream_socket = [ `stream ] socket
 
 type listen_opts = {
   reuse_addr : bool;
--- riot/riot.mli
+++ riot/riot.mli
@@ -437,7 +437,7 @@ module Net : sig
   end
 
   module Socket : sig
-    type 'kind socket
+    type 'kind socket = Fd.t
     type listen_socket = [ `listen ] socket
     type stream_socket = [ `stream ] socket
 

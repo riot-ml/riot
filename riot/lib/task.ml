26b29dd79e4212b67ceee801d1d635483478c57b
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-03 19:04:35 +0100

    fix: our receive refs don't yet work exactly as erlang refs

--- riot/lib/task.ml
+++ riot/lib/task.ml
@@ -25,7 +25,7 @@ let rec await :
     type res.
     ?timeout:int64 -> res t -> (res, [> `Timeout | `Process_down ]) result =
  fun ?timeout:after t ->
-  match receive ?after ~ref:t.ref () with
+  match receive ?after () with
   | exception Receive_timeout -> Error `Timeout
   | Reply (ref', res) when Ref.equal t.ref ref' -> (
       match Ref.type_equal t.ref ref' with

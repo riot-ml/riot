cf4ae9a9e3aad3c8dd0071fed967d224080465cf
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-05 14:12:00 +0100

    feat: compile patterns to stirng construction sequences

--- bytestring/ppx_bytestring.ml
+++ bytestring/ppx_bytestring.ml
@@ -1,22 +1,20 @@
 open Ppxlib
-open Ast_builder.Default
+open Ast_helper
 
-let tag = "bin"
+let tag = "bytestring"
 
-(*
-let mk_expression ~ctxt expr = 
+let mk_expression ~ctxt:_ expr =
+  let loc = expr.pexp_loc in
   match expr.pexp_desc with
-  | Pexp_constant (Pconst_string (value, _, _)) -> ()
+  | Pexp_constant (Pconst_string (value, _, _)) ->
+      let bytepattern = Bytepattern.(parse value |> lower) in
+      Bytepattern.to_transient_builder ~loc bytepattern
+  | _ -> expr
 
-let expression_rule = Extension.V3.declare
-  tag
-  Extension.Context.expression
-  Ast_pattern.(single_expr_payload __)
-  mk_expression
+let expression_rule =
+  Extension.V3.declare tag Extension.Context.expression
+    Ast_pattern.(single_expr_payload __)
+    mk_expression
   |> Context_free.Rule.extension
 
-let () =
-  Driver.register_transformation tag ~rules:[
-    expression_rule
-  ]
-  *)
+let () = Driver.register_transformation tag ~rules:[ expression_rule ]

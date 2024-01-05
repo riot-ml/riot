9e74dfef752d15f548acea25ad26ff3fb8645d88
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-05 03:59:57 +0100

    sketch iter/builder interface

--- binstring/binstring.mli
+++ binstring/binstring.mli
@@ -4,18 +4,18 @@ val empty : t
 val length : t -> int
 
 exception Malformed of string
-
 val of_string : string -> t
 val to_string : t -> string
 
 exception View_out_of_bounds
-
-val sub : ?off:int -> len:int -> t -> t
+val view : ?off:int -> len:int -> t -> t
 val join : t -> t -> t
 val ( ^ ) : t -> t -> t
 val concat : t -> t list -> t
 
-module Extract : sig
+module Iter : sig
+  type string = t
+  type t 
   exception Invalid_position
   exception Byte_not_found
 
@@ -23,3 +23,11 @@ module Extract : sig
   val get_byte : t -> int -> char
 end
 
+
+module Builder : sig
+  type string = t
+  type t
+
+  val add_string : string -> t -> t
+  val build : t -> string
+end

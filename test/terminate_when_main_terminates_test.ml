a84a7948f8f7cf3365d69c0a1192fd33a6f4860a
Diogo Mafra <diogo@mafra.sh>
2024-01-05 19:20:54 -0300

    test: adding print to test termination

--- test/terminate_when_main_terminates_test.ml
+++ test/terminate_when_main_terminates_test.ml
@@ -1,14 +1,7 @@
-[@@@warning "-8"]
-
-open Riot
-
-exception Error
-
-let main () =
-  (* spawn process that should throw which should terminate main process *)
-  let _pid = spawn_link (fun () -> raise Error) in
-  ()
-
 let () =
-  Logger.set_log_level (Some Info);
-  Riot.run @@ main
+  (* start the runtime with a function that will immediately return *)
+  Riot.run @@ fun () ->
+  ();
+(* print that everything is OK *)
+  print_string "termination_test: OK"
+

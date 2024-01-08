895c1e65f57afc7dadcf4f249a2c7007e4a1dc67
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-08 09:27:16 +0100

    test: Add tests for string ops, iter, and transient

--- bytestring/bytestring.mli
+++ bytestring/bytestring.mli
@@ -14,15 +14,13 @@ val to_string : t -> string
 
 exception View_out_of_bounds
 
-(* XXX: delete view in favor of sub since it takes t and returns t *)
-val view : ?off:int -> len:int -> t -> t
 val join : t -> t -> t
 val ( ^ ) : t -> t -> t
 val concat : t -> t list -> t
 val sub : ?off:int -> len:int -> t -> t
 
 module Iter : sig
-  type string = t
+  type bytestring = t
   type t
 
   exception Invalid_position
@@ -30,32 +28,32 @@ module Iter : sig
 
   val next_bit : t -> int
   val next_bits : size:int -> t -> int
-  val next_byte : t -> string
-  val next_bytes : size:int -> t -> string
-  val next_utf8 : t -> string
-  val next_utf8_seq : len:int -> t -> string
-  val rest : t -> string
+  val next_byte : t -> bytestring
+  val next_bytes : size:int -> t -> bytestring
+  val next_utf8 : t -> bytestring
+  val next_utf8_seq : len:int -> t -> bytestring
+  val rest : t -> bytestring
   val expect_empty : t -> unit
   val expect_bits : t -> int -> unit
-  val expect_bytes : t -> string -> unit
+  val expect_bytes : t -> bytestring -> unit
   val expect_literal_int : t -> ?size:int -> int -> unit
-  val expect_literal_string : t -> ?size:string -> Stdlib.String.t -> unit
+  val expect_literal_string : t -> ?size:int -> Stdlib.String.t -> unit
 end
 
 val to_iter : t -> Iter.t
 
 module Transient : sig
-  type string = t
+  type bytestring = t
   type t
 
   val create : unit -> t
-  val add_string : t -> ?size:int -> string -> unit
+  val add_string : t -> ?size:int -> bytestring -> unit
   val add_bits : t -> ?size:int -> int -> unit
-  val add_utf8 : t -> ?size:int -> string -> unit
+  val add_utf8 : t -> ?size:int -> bytestring -> unit
   val add_literal_int : t -> ?size:int -> int -> unit
   val add_literal_utf8 : t -> ?size:int -> Stdlib.String.t -> unit
   val add_literal_string : t -> ?size:int -> Stdlib.String.t -> unit
-  val commit : t -> string
+  val commit : t -> bytestring
 end
 
 val to_transient : t -> Transient.t

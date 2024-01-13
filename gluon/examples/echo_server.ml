5f19e3a2c5275f5ccb408710be5f097bb5cfaed0
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-13 21:17:31 +0100

    feat(gluon): get an tcp echo server running fast

---
+++ gluon/examples/echo_server.ml
@@ -0,0 +1,113 @@
+open Gluon
+open Gluon_events
+open Gluon_common
+
+(* let log = Format.printf *)
+
+let handle_error r =
+  match r with
+  | Ok x -> x
+  | Error `Would_block -> Printf.sprintf "Would block\r\n%!" |> failwith
+  | Error `No_info -> Printf.sprintf "No info\r\n%!" |> failwith
+  | Error `Connection_closed ->
+      Printf.sprintf "Connection closed\r\n%!" |> failwith
+  | Error (`Exn exn) ->
+      Printf.sprintf "Exn: %S\r\n%!" (Printexc.to_string exn) |> failwith
+  | Error (`Unix_error err) ->
+      Printf.sprintf "Unix error: %S\r\n%!" (Unix.error_message err) |> failwith
+
+let run () =
+  let addr = Net.Addr.(tcp loopback 9001) in
+
+  let* poll = Poll.make () in
+  let* server = Net.Tcp_listener.bind addr in
+
+  let server_token = Token.next () in
+  let* () =
+    Poll.register poll server_token Interest.readable
+      (Net.Tcp_listener.to_source server)
+  in
+
+  let connections : (Token.t, Net.Tcp_stream.t) Hashtbl.t =
+    Hashtbl.create 1024
+  in
+
+  let accept () =
+    (* log "accepting connection\n%!"; *)
+    let* conn, adddr = Net.Tcp_listener.accept server in
+    let token = Token.next () in
+    (* log "accepted %a with %a\n%!" Net.Addr.pp addr Token.pp token; *)
+    let* () =
+      Poll.register poll token
+        Interest.(add readable writable)
+        (Net.Tcp_stream.to_source conn)
+    in
+    Hashtbl.replace connections token conn;
+    Ok ()
+  in
+
+  let rec read_write conn event token =
+    if Event.is_readable event then
+      (* log "event is readable \n%!"; *)
+      let rec echo_loop src =
+        (* log "echo loop\n%!"; *)
+        let capacity = 1024 * 50 in
+        let* data =
+          Bytestring.with_bytes ~capacity (Net.Tcp_stream.read conn)
+        in
+        (* log "read %d into buffer\n%!" len; *)
+        (* log "subbed buffer\n%!"; *)
+        (* log "made %d bytestring \n%!" (Bytestring.length data); *)
+        let* () =
+          if Bytestring.length data = 0 then Error `Connection_closed else Ok ()
+        in
+        let* written = Net.Tcp_stream.writev conn (Bytestring.to_iovec data) in
+        (* log "write %d bytes \n%!" written; *)
+        let* () =
+          Poll.reregister poll token Interest.(add readable writable) src
+        in
+        echo_loop src
+      in
+      let src = Net.Tcp_stream.to_source conn in
+      match echo_loop src with
+      | Ok _ | Error `Would_block -> `continue
+      | Error _ -> `finished
+    else (* log "event is not writable/readable? \n%!"; *)
+      `continue
+  in
+
+  let handle_client event token =
+    (* log "handling client event with %a\n%!" Token.pp token; *)
+    match Hashtbl.find_opt connections token with
+    | None ->
+        (* log "no connection found for token %a\n%!" Token.pp token; *)
+        Ok ()
+    | Some conn ->
+        (* log "found connection %a\n%!" Net.Tcp_stream.pp conn; *)
+        if read_write conn event token = `finished then (
+          (* log "deregistering connection %a\n%!" Net.Tcp_stream.pp conn; *)
+          Hashtbl.remove connections token;
+          Poll.deregister poll (Net.Tcp_stream.to_source conn))
+        else Ok ()
+  in
+
+  let handle_event (event : Event.t) =
+    let token = Event.token event in
+    (* log "handling event with %a\n%!" Token.pp token; *)
+    log "(server_token=%a)\n%!" Token.pp server_token;
+    if Token.equal token server_token then accept ()
+    else handle_client event token
+  in
+
+  let rec poll_loop () =
+    let* events = Poll.poll ~max_events:100 poll in
+    if List.length events > 0 then
+      (* log "polled %d events\n%!" (List.length events); *)
+      List.iter (fun e -> handle_event e |> handle_error) events;
+    poll_loop ()
+  in
+
+  (* log "polling...\n%!"; *)
+  poll_loop ()
+
+let () = handle_error (run ())
--- gluon/sys/unix/kqueue.ml
+++ gluon/sys/unix/kqueue.ml
@@ -2,22 +2,18 @@ open Gluon_common
 
 type kevent
 type kqueue = Fd.t
+type event = { fd : Fd.t; filter : int; flags : int; token : int }
 
 module FFI : sig
   val fcntl : kqueue -> int -> int -> int io_result
-  val kevent_create : kqueue -> filter:int -> flags:int -> token:int -> kevent
-  val kevent_register : kqueue -> kevent array -> int array -> unit io_result
+  val kevent_register : kqueue -> event array -> int array -> unit io_result
   val kqueue : unit -> kqueue io_result
 
   val kevent :
-    max_events:int -> timeout:int64 -> kqueue -> kevent array io_result
-
-  val kevent_udata : kevent -> int
-  val kevent_filter : kevent -> int
-  val kevent_flags : kevent -> int
+    max_events:int -> timeout:int64 -> kqueue -> event array io_result
 end = struct
   external gluon_unix_kevent :
-    max_events:int -> timeout:int64 -> kqueue -> kevent array
+    max_events:int -> timeout:int64 -> kqueue -> event array
     = "gluon_unix_kevent"
 
   let kevent ~max_events ~timeout kq =
@@ -33,38 +29,25 @@ end = struct
   let fcntl fd cmd arg = syscall @@ fun () -> gluon_unix_fcntl fd ~cmd ~arg
 
   external gluon_unix_kevent_register :
-    kqueue -> kevent array -> int array -> unit = "gluon_unix_kevent_register"
+    kqueue -> event array -> int array -> unit = "gluon_unix_kevent_register"
 
   let kevent_register fd changes ignored_errors =
     syscall @@ fun () -> gluon_unix_kevent_register fd changes ignored_errors
-
-  external gluon_unix_kevent_create :
-    Fd.t -> filter:int -> flags:int -> token:int -> kevent
-    = "gluon_unix_kevent_create"
-
-  let kevent_create kq ~filter ~flags ~token =
-    gluon_unix_kevent_create kq ~filter ~flags ~token
-
-  external gluon_unix_kevent_udata : kevent -> int = "gluon_unix_kevent_udata"
-
-  let kevent_udata kev = gluon_unix_kevent_udata kev
-
-  external gluon_unix_kevent_filter : kevent -> int = "gluon_unix_kevent_filter"
-
-  let kevent_filter kev = gluon_unix_kevent_filter kev
-
-  external gluon_unix_kevent_flags : kevent -> int = "gluon_unix_kevent_flags"
-
-  let kevent_flags kev = gluon_unix_kevent_flags kev
 end
 
 module Event = struct
-  type t = kevent
+  type t = event
 
-  let filter t = FFI.kevent_filter t
-  let flags t = FFI.kevent_flags t
-  let token t = Token.of_int (FFI.kevent_udata t)
-  let is_readable t = filter t = Libc.evfilt_read
+  let make fd ~filter ~flags ~token = { fd; filter; flags; token }
+  let filter t = t.filter
+  let flags t = t.flags
+  let token t = Token.of_int t.token
+
+  let is_readable t =
+    let f = filter t in
+    log "is_readable? %d == %d\n" f Libc.evfilt_read;
+    filter t = Libc.evfilt_read
+
   let is_writable t = filter t = Libc.evfilt_write
   let is_error t = flags t land Libc.ev_error != 0
   let is_read_closed t = is_readable t && flags t land Libc.ev_eof != 0
@@ -89,27 +72,22 @@ module Selector = struct
     Ok events
 
   let register t ~fd ~token ~interest =
-    let tok = token in
     let token = Token.to_int token in
     let flags = Libc.(ev_clear lor ev_receipt lor ev_add) in
     let changes = ref [] in
 
-    if Interest.is_writable interest then (
-      Format.printf "%a registering writeable interest for %a\r\n%!" Token.pp tok Fd.pp fd;
-      let kevent =
-        FFI.kevent_create fd ~filter:Libc.evfilt_write ~flags ~token
-      in
-      changes := kevent :: !changes);
+    (if Interest.is_writable interest then
+       (* log "%a registering writeable interest for %a\r\n%!" Token.pp tok Fd.pp fd; *)
+       let kevent = Event.make fd ~filter:Libc.evfilt_write ~flags ~token in
+       changes := kevent :: !changes);
 
-    if Interest.is_readable interest then (
-      Format.printf "%a registering readable interest for %a\r\n%!" Token.pp tok Fd.pp fd;
-      let kevent =
-        FFI.kevent_create fd ~filter:Libc.evfilt_read ~flags ~token
-      in
-      changes := kevent :: !changes);
+    (if Interest.is_readable interest then
+       (* log "%a registering readable interest for %a\r\n%!" Token.pp tok Fd.pp fd; *)
+       let kevent = Event.make fd ~filter:Libc.evfilt_read ~flags ~token in
+       changes := kevent :: !changes);
 
     let changes = Array.of_list !changes in
-    Format.printf "%a registering %a\r\n%!" Token.pp tok Fd.pp fd;
+    (* log "%a registering %a\r\n%!" Token.pp tok Fd.pp fd; *)
     FFI.kevent_register t.kq changes [| Libc.epipe |]
 
   let reregister t ~fd ~token ~interest =
@@ -128,22 +106,22 @@ module Selector = struct
 
     let changes =
       [|
-        FFI.kevent_create fd ~filter:Libc.evfilt_write ~flags:write_flags ~token;
-        FFI.kevent_create fd ~filter:Libc.evfilt_read ~flags:read_flags ~token;
+        Event.make fd ~filter:Libc.evfilt_write ~flags:write_flags ~token;
+        Event.make fd ~filter:Libc.evfilt_read ~flags:read_flags ~token;
       |]
     in
 
-    Format.printf "reregistering %a\r\n%!" Fd.pp fd;
+    (* log "reregistering %a\r\n%!" Fd.pp fd; *)
     FFI.kevent_register t.kq changes Libc.[| epipe; enoent |]
 
   let deregister t ~fd =
     let flags = Libc.(ev_delete lor ev_receipt) in
     let changes =
       [|
-        FFI.kevent_create fd ~filter:Libc.evfilt_write ~flags ~token:0;
-        FFI.kevent_create fd ~filter:Libc.evfilt_read ~flags ~token:0;
+        Event.make fd ~filter:Libc.evfilt_write ~flags ~token:0;
+        Event.make fd ~filter:Libc.evfilt_read ~flags ~token:0;
       |]
     in
-    Format.printf "deregistering %a\r\n%!" Fd.pp fd;
+    (* log "deregistering %a\r\n%!" Fd.pp fd; *)
     FFI.kevent_register t.kq changes Libc.[| enoent |]
 end

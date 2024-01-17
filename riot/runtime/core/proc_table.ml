fd05a1ab1734709efb91a34fbfaca638cb6f1bd7
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-17 14:55:30 +0100

    fix(riot): unlock proc_table on reregistration errors

--- riot/runtime/core/proc_table.ml
+++ riot/runtime/core/proc_table.ml
@@ -2,14 +2,14 @@ type t = { processes : (Pid.t, Process.t) Hashtbl.t; lock : Mutex.t }
 
 let create () = { lock = Mutex.create (); processes = Hashtbl.create 16_000 }
 let get t pid = Hashtbl.find_opt t.processes pid
+let remove t pid = Hashtbl.remove t.processes pid
 
 exception Reregistering_process of Process.t
 
 let register_process t (proc : Process.t) =
-  Mutex.lock t.lock;
+  Mutex.protect t.lock @@ fun () ->
   let pid = Process.pid proc in
   if Hashtbl.mem t.processes pid then raise (Reregistering_process proc)
-  else Hashtbl.add t.processes pid proc;
-  Mutex.unlock t.lock
+  else Hashtbl.replace t.processes pid proc
 
 let processes t = Hashtbl.to_seq t.processes

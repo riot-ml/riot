39a1ed4776f2f3e2e9d7e52277fc38b7bdf0e504
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-17 15:25:23 +0100

    fix(riot): expose proc table remove

--- riot/runtime/core/proc_table.mli
+++ riot/runtime/core/proc_table.mli
@@ -6,3 +6,4 @@ val create : unit -> t
 val get : t -> Pid.t -> Process.t option
 val register_process : t -> Process.t -> unit
 val processes : t -> (Pid.t * Process.t) Seq.t
+val remove : t -> Pid.t -> unit

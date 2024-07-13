1a034a0b32e2b83739a6c14f41063af6b3013006
Dillon Mulroy <dillon.mulroy@gmail.com>
2024-07-13 16:46:22 -0400

    Use nix-ocaml overlays + fmt nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "An actor-model multi-core scheduler for OCaml 5";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
     bytestring = {
       url = "github:riot-ml/bytestring";
@@ -47,68 +47,78 @@
     };
   };
 
-  outputs = inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-          inherit (pkgs) ocamlPackages mkShell;
-          inherit (ocamlPackages) buildDunePackage;
-          version = "0.0.9+dev";
-        in
-          {
-            devShells = {
-              default = mkShell.override {stdenv = pkgs.clang17Stdenv;} {
-                buildInputs = with ocamlPackages; [
-                  dune_3
-                  ocaml
-                  utop
-                  ocamlformat
-                ];
-                inputsFrom = [ self'.packages.default ];
-                packages = builtins.attrValues {
-                  inherit (pkgs) clang_17 clang-tools_17 pkg-config;
-                  inherit (ocamlPackages) ocaml-lsp ocamlformat-rpc-lib;
-                };
-              };
-            };
-            packages = {
-              randomconv = buildDunePackage {
-                version = "0.2.0";
-                pname = "randomconv";
-                src = builtins.fetchGit {
-                  url = "git@github.com:hannesm/randomconv.git";
-                  rev = "b2ce656d09738d676351f5a1c18aff0ff37a7dcc";
-                  ref = "refs/tags/v0.2.0";
-                };
-              };
-
-              default = buildDunePackage {
-                inherit version;
-                pname = "riot";
-                propagatedBuildInputs = with ocamlPackages; [
-                  inputs'.bytestring.packages.default
-                  inputs'.castore.packages.default
-                  inputs'.config.packages.default
-                  inputs'.gluon.packages.default
-                  inputs'.rio.packages.default
-                  (mdx.override {
-                    inherit logs;
-                  })
-                  mirage-crypto
-                  mirage-crypto-rng
-                  mtime
-                  odoc
-                  ptime
-                  self'.packages.randomconv
-                  inputs'.telemetry.packages.default
-                  tls
-                  uri
-                  x509
-                ];
-                src = ./.;
-              };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        inherit (pkgs) ocamlPackages mkShell;
+        inherit (ocamlPackages) buildDunePackage;
+        version = "0.0.9+dev";
+      in {
+        devShells = {
+          default = mkShell.override {stdenv = pkgs.clang17Stdenv;} {
+            buildInputs = with ocamlPackages; [
+              dune_3
+              ocaml
+              utop
+              ocamlformat
+            ];
+            inputsFrom = [self'.packages.default];
+            packages = builtins.attrValues {
+              inherit (pkgs) clang_17 clang-tools_17 pkg-config;
+              inherit (ocamlPackages) ocaml-lsp ocamlformat-rpc-lib;
             };
           };
+        };
+        packages = {
+          randomconv = buildDunePackage {
+            version = "0.2.0";
+            pname = "randomconv";
+            src = builtins.fetchGit {
+              url = "git@github.com:hannesm/randomconv.git";
+              rev = "b2ce656d09738d676351f5a1c18aff0ff37a7dcc";
+              ref = "refs/tags/v0.2.0";
+            };
+          };
+
+          default = buildDunePackage {
+            inherit version;
+            pname = "riot";
+            propagatedBuildInputs = with ocamlPackages; [
+              inputs'.bytestring.packages.default
+              inputs'.castore.packages.default
+              inputs'.config.packages.default
+              inputs'.gluon.packages.default
+              inputs'.rio.packages.default
+              (mdx.override {
+                inherit logs;
+              })
+              mirage-crypto
+              mirage-crypto-rng
+              mtime
+              odoc
+              ptime
+              self'.packages.randomconv
+              inputs'.telemetry.packages.default
+              tls
+              uri
+              x509
+            ];
+            src = ./.;
+          };
+        };
+        formatter = pkgs.alejandra;
+      };
     };
 }
